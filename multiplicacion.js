d916aec638d85d52df96c6a1e73a528eb438e5ae
darrell <jafet.dna@gmail.com>
2023-07-25 17:43:30 -0600

    Revert "Mensaje del commit"
    This reverts commit fc20002af952347e36ecb6d454f46d6f4502e7d6.

--- multiplicacion.js
+++
@@ -1,6 +0,0 @@
-import chalk from 'chalk';
-
-export default function multiplicacion(a, b) {
-  const resultado = a * b;
-  console.log(chalk.blue(`La multiplicación es: ${resultado}`));
-}
--- suma.js
+++
@@ -1,6 +0,0 @@
-import chalk from 'chalk';
-
-export default function suma(a, b) {
-  const resultado = a + b;
-  console.log(chalk.red(`La suma es: ${resultado}`));
-}
