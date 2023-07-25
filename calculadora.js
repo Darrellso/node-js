ae9565f4f64af9241bd798feeb6f5ad6064a0861
darrell <jafet.dna@gmail.com>
2023-07-25 17:43:45 -0600

    Revert "Revert "Mensaje del commit""
    This reverts commit d916aec638d85d52df96c6a1e73a528eb438e5ae.

---
+++ calculadora.js
@@ -0,0 +1,13 @@
+import  suma  from './suma.js';
+import  resta  from './resta.js';
+import  multiplicacion  from './multiplicacion.js';
+import  division  from './division.js';
+
+// Ejemplo
+const a = 10;
+const b = 5;
+
+suma(a, b);
+resta(a, b);
+multiplicacion(a, b);
+division(a, b);
