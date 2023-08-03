7a06dcec850939bc565d1bf4212b4fe26390cfbc
darrell <jafet.dna@gmail.com>
2023-08-03 13:13:30 -0600

    ..

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// servers.js (Archivo del servidor)
 
 const express = require('express');
 const app = express();
@@ -7,15 +6,14 @@ const mongoose = require('mongoose');
 const mongoosePaginate = require('mongoose-paginate-v2');
 
 const PORT = 3000;
-const MONGODB_URI = 'mongodb+srv://samael:cetav123@cluster0.ox7yybk.mongodb.net/'; // URL de la base de datos MongoDB
-const MAX_RETRIES = 3; // Número máximo de intentos de reconexión
+const MONGODB_URI = 'mongodb+srv://samael:cetav123@cluster0.ox7yybk.mongodb.net/';
+const MAX_RETRIES = 3; 
 let retries = 0;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Definir el esquema de la carta
 const cardSchema = new mongoose.Schema({
   cardType: String,
   cardName: String,
@@ -23,10 +21,8 @@ const cardSchema = new mongoose.Schema({
   cardBattlePoints: Number,
 });
 
-// Agregar el plugin de paginación al esquema
 cardSchema.plugin(mongoosePaginate);
 
-// Crear el modelo de la carta
 const Card = mongoose.model('Card', cardSchema);
 
 function connectWithRetry() {
@@ -42,29 +38,27 @@ function connectWithRetry() {
       retries++;
       if (retries < MAX_RETRIES) {
         console.log(`Intentando reconectar a MongoDB (Intento ${retries})...`);
-        setTimeout(connectWithRetry, 5000); // Esperar 5 segundos antes de intentar reconectar
+        setTimeout(connectWithRetry, 5000); 
       } else {
         console.error('Se superó el número máximo de intentos de reconexión. Saliendo...');
-        process.exit(1); // Salir del proceso de Node.js con código de error 1
+        process.exit(1); 
       }
     });
 }
 
 
-// Ruta para buscar cartas por nombre o tipo
 app.post('/cards/search', async (req, res) => {
   try {
-    const searchTerm = req.body.searchTerm.toLowerCase(); // Convertir a minúsculas
+    const searchTerm = req.body.searchTerm.toLowerCase(); 
 
-    // Realizar la búsqueda en la base de datos utilizando Mongoose
     const results = await Card.find({
       $or: [
-        { cardName: { $regex: searchTerm, $options: 'i' } }, // Búsqueda por nombre
-        { cardType: { $regex: searchTerm, $options: 'i' } }, // Búsqueda por tipo
+        { cardName: { $regex: searchTerm, $options: 'i' } }, 
+        { cardType: { $regex: searchTerm, $options: 'i' } }, 
       ],
     });
 
-    res.json(results); // Responder con los resultados en formato JSON
+    res.json(results);
   } catch (error) {
     console.error('Error al buscar cartas:', error);
     res.status(500).json({ error: 'Error al buscar cartas' });
@@ -72,7 +66,6 @@ app.post('/cards/search', async (req, res) => {
 });
 
 
- // Ruta para obtener todas las cartas con paginación, ordenamiento y filtrado
  app.get('/cards', async (req, res) => {
   try {
     const { page, limit, sortBy, sortOrder, cardType } = req.query;
@@ -97,7 +90,6 @@ app.post('/cards/search', async (req, res) => {
 });
 
 
- // Ruta para registrar una nueva carta
 app.post('/cards', async (req, res) => {
   try {
     const newCard = new Card(req.body);
@@ -110,7 +102,6 @@ app.post('/cards', async (req, res) => {
 });
 
 
-// Ruta para editar una carta
 
 app.put('/cards/:id', async (req, res) => {
   try {
@@ -127,7 +118,6 @@ app.put('/cards/:id', async (req, res) => {
   }
 });
 
-// Ruta para eliminar una carta
 app.delete('/cards/:id', async (req, res) => {
   try {
     const cardId = req.params.id;
@@ -159,14 +149,12 @@ app.get('/cards/:id', async (req, res) => {
 
 
 
-// Cargar la página de inicio
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Iniciar el servidor y la conexión a MongoDB
 function startServer() {
-  connectWithRetry(); // Iniciar la conexión a la base de datos con reintentos
+  connectWithRetry(); 
 }
 
 startServer();
